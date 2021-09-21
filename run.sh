2d88497fca16b7f4b737c8be151faf0265317e08
Alex Tu <alex.tu@canonical.com>
2021-09-22 01:50:29 +0800

    testing, in

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+docker build .  -t localhost:5000/test-resource
+docker push localhost:5000/test-resource
+sleep 3
 fly -t demo set-pipeline -p hello-resource -c hello-pipeline.yml
 fly -t demo unpause-pipeline -p hello-resource
 fly -t demo trigger-job -j hello-resource/hello-job -w
